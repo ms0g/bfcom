508768e67acbd64cc25d31a9d14377603473e97e
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-07-22 16:58:11 +0300

    refactor(all): init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bfcom
+
+A tiny Brainfuck interpreter for MS-DOS `.COM` executables.  
+Built for real-mode x86 systems — because why not run esoteric code on ancient machines?
+
+## Usage
+```bash
+C:\>BF.COM HELLO.BF
+Hello, World!
+```
+## License
+This project is licensed under the  BSD 3-Clause License. See the LICENSE file for details.
+
---
+++ SRC/INTERPRT.C
@@ -0,0 +1,65 @@
+
+#define TAPE_SIZE 30000
+
+unsigned char tape[TAPE_SIZE] = {0};
+
+void interpret(char* code) {
+    const char* pc = code;
+    unsigned char* ptr = tape;
+    
+    while (*pc) {
+        switch (*pc) {
+            case '>': ptr++; break;
+            case '<': ptr--; break;
+            case '+': (*ptr)++; break;
+            case '-': (*ptr)--; break;
+            case '.': {
+                unsigned char chr = *ptr;
+
+                asm {
+                    mov dl, chr
+                    mov ah, 2
+                    int 0x21
+                }
+                break;
+            }
+            case ',': {
+                unsigned char chr;
+                asm {
+                    mov ah, 1
+                    int 0x21
+                    mov chr, al
+                }
+                *ptr = chr;
+                break;
+            }
+            case '[': {
+                if (!*ptr) {
+                    int depth = 1;
+
+                    while (depth > 0 && *++pc) {
+                        if (*pc == '[') 
+                            depth++;
+                        else if (*pc == ']') 
+                            depth--;
+                    }
+                }
+                break;
+            }
+            case ']': {
+                if (*ptr) {
+                    int depth = 1;
+
+                    while (depth > 0 && --pc >= code) {
+                        if (*pc == ']') 
+                            depth++;
+                        else if (*pc == '[') 
+                            depth--;
+                    }
+                }
+                break;
+            }
+        }
+        pc++;
+    }
+}
